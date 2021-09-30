b606351120144d0ff7cb728746131da31222864e
Rico <drraccoony@gmail.com>
2021-09-29 21:52:24 -0500

    Playing with fetch return

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 $("#searchBtn").click(function () {
     var selectedState = $('#states').val();
-    alert("You picked " + selectedState + "\n\nSee web console for API object return.");
 
-    // TODO: Setup fetch
-    // TODO: Pass state into fetch
+    // TODO: Fetch parks
+    fetch('https://developer.nps.gov/api/v1/parks?stateCode=' +
+    selectedState +
+    '&api_key=KrGPbUdmA0msAP4qXYSGzKcU89oOOcHNzdJ7Gpek')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // JSON data parsed by `data.json()` call
+            alert("You picked " + selectedState + "\n\nSee web console for complete API object return.\n\nTotal Parks: " + data.total );
+        })
+        .catch((error) => {
+            console.error('Error: ', error);
+        });
+
+    
 });
