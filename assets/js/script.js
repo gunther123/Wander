d9b50809c37238dc078f067005ec3dd32b1733a7
Rico <drraccoony@gmail.com>
2021-09-29 21:07:22 -0500

    Changed from let to var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $("#searchBtn").click(function () {
-    let selectedState = $('#states').val();
+    var selectedState = $('#states').val();
     alert("You picked " + selectedState + "\n\nSee web console for API object return.");
 
     // TODO: Setup fetch
