a4fdfec6a774794afcebfcdc697f5a284caa5760
Rico <drraccoony@gmail.com>
2021-09-30 20:07:10 -0500

    Added a Tailwind Navigation Bar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem'
+        },
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
